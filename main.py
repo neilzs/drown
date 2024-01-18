c96b09aa0b2af575493e363d0ba0b8f3eb3de535
yea <neils@outlook.co.id>
2024-01-19 02:41:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # coding:utf-8
 from kivy.app import App
 from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 import cv2
@@ -10,7 +12,16 @@ class KivyCamera(Image):
     def __init__(self, capture, fps, **kwargs):
         super(KivyCamera, self).__init__(**kwargs)
         self.capture = capture
-        Clock.schedule_interval(self.update, 1.0 / fps)
+        self.fps = fps
+        self.is_capturing = False
+
+    def start_capture(self):
+        Clock.schedule_interval(self.update, 1.0 / self.fps)
+        self.is_capturing = True
+
+    def stop_capture(self):
+        Clock.unschedule(self.update)
+        self.is_capturing = False
 
     def update(self, dt):
         ret, frame = self.capture.read()
@@ -25,16 +36,29 @@ class KivyCamera(Image):
             self.texture = image_texture
 
 
-class CamApp(App):
+class CameraApp(App):
     def build(self):
         self.capture = cv2.VideoCapture(0)
         self.my_camera = KivyCamera(capture=self.capture, fps=30)
-        return self.my_camera
+
+        # Add a start button
+        self.start_button = Button(text='Start Camera', on_press=self.start_camera)
+
+        # Create a layout to hold the button and camera preview
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(self.start_button)
+        layout.add_widget(self.my_camera)
+
+        return layout
 
     def on_stop(self):
-        #without this, app will not exit even if the window is closed
+        # without this, the app will not exit even if the window is closed
         self.capture.release()
+
+    def start_camera(self, instance):
+        # Start the camera when the button is pressed
+        self.my_camera.start_capture()
 
 
 if __name__ == '__main__':
-    CamApp().run()
+    CameraApp().run()

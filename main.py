35ce2f5990ab8fe580aec4588626c4593c9d57b2
yea <neils@outlook.co.id>
2024-01-18 23:53:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
-# Import kivy
+# coding:utf-8
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+import cv2
 
-# Membuat kelas aplikasi
-class HelloWorldApp(App):
+
+class KivyCamera(Image):
+    def __init__(self, capture, fps, **kwargs):
+        super(KivyCamera, self).__init__(**kwargs)
+        self.capture = capture
+        Clock.schedule_interval(self.update, 1.0 / fps)
+
+    def update(self, dt):
+        ret, frame = self.capture.read()
+        if ret:
+            # convert it to texture
+            buf1 = cv2.flip(frame, 0)
+            buf = buf1.tostring()
+            image_texture = Texture.create(
+                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            # display image from the texture
+            self.texture = image_texture
+
+
+class CamApp(App):
     def build(self):
-        # Membuat tata letak (layout) dengan kotak (Box)
-        layout = BoxLayout(orientation='vertical')
+        self.capture = cv2.VideoCapture(0)
+        self.my_camera = KivyCamera(capture=self.capture, fps=30)
+        return self.my_camera
 
-        # Menambahkan label ke layout
-        label = Label(text='Hello, World!')
-        layout.add_widget(label)
+    def on_stop(self):
+        #without this, app will not exit even if the window is closed
+        self.capture.release()
 
-        # Menambahkan tombol ke layout
-        button = Button(text='Click Me!', on_press=self.on_button_click)
-        layout.add_widget(button)
 
-        # Mengembalikan layout sebagai antarmuka pengguna aplikasi
-        return layout
-
-    # Fungsi yang akan dipanggil saat tombol ditekan
-    def on_button_click(self, instance):
-        print('Button Clicked!')
-
-# Membuat instance aplikasi dan menjalankannya
 if __name__ == '__main__':
-    HelloWorldApp().run()
+    CamApp().run()

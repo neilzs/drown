1276ed5e485c4930ab7f0121f0e9fdeec51fe14a
yea <neils@outlook.co.id>
2024-01-18 22:25:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,40 @@
+# coding:utf-8
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.clock import Clock
 from kivy.graphics.texture import Texture
-from kivy.uix.camera import Camera
-from kivy.lang import Builder
-import numpy as np
 import cv2
 
 
-Builder.load_file("myapplayout.kv")
+class KivyCamera(Image):
+    def __init__(self, capture, fps, **kwargs):
+        super(KivyCamera, self).__init__(**kwargs)
+        self.capture = capture
+        Clock.schedule_interval(self.update, 1.0 / fps)
 
-class AndroidCamera(Camera):
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    camera_resolution = (640, 480)
-    face_resolution = (128, 96)
-    ratio = camera_resolution[0] / face_resolution[0]
-
-
-    def _camera_loaded(self, *largs):
-        self.texture = Texture.create(size=np.flip(self.camera_resolution), colorfmt='rgb')
-        self.texture_size = list(self.texture.size)
-
-    def on_tex(self, *l):
-        if self._camera._buffer is None:
-            return None
-        frame = self.frame_from_buf()
-
-        self.frame_to_screen(frame)
-        super(AndroidCamera, self).on_tex(*l)
-
-    def frame_from_buf(self):
-        w, h = self.resolution
-        frame = np.frombuffer(self._camera._buffer.tostring(), 'uint8').reshape((h + h // 2, w))
-        frame_bgr = cv2.cvtColor(frame, 93)
-        return np.rot90(frame_bgr, 3)
-
-    def frame_to_screen(self, frame):
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        self.face_det(frame_rgb)
-
-        flipped = np.flip(frame_rgb, 0)
-        buf = flipped.tostring()
-        self.texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
-
-    def face_det(self, frame):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        resized = cv2.resize(gray, (self.face_resolution[1], self.face_resolution[0]))
-        faces = self.face_cascade.detectMultiScale(resized, 1.3, 2)
-        if len(faces) != 0:
-            face = faces[np.argmax(faces[:, 3])]
-            x, y, w, h = face
-            cv2.rectangle(frame, (int(x * self.ratio), int(y * self.ratio)), (int((x + w) * self.ratio), int((y + h) * self.ratio)), (0, 255, 0), 2)
+    def update(self, dt):
+        ret, frame = self.capture.read()
+        if ret:
+            # convert it to texture
+            buf1 = cv2.flip(frame, 0)
+            buf = buf1.tostring()
+            image_texture = Texture.create(
+                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            # display image from the texture
+            self.texture = image_texture
 
 
-
-class MyLayout(BoxLayout):
-    pass
-
-
-class MyApp(App):
+class CamApp(App):
     def build(self):
-        return MyLayout()
+        self.capture = cv2.VideoCapture(0)
+        self.my_camera = KivyCamera(capture=self.capture, fps=30)
+        return self.my_camera
+
+    def on_stop(self):
+        #without this, app will not exit even if the window is closed
+        self.capture.release()
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    CamApp().run()
